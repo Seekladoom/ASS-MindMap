e60b6fd589ece42b8c900cc24f31002b07da565a
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2020-10-29 02:07:55 +0800

    Add files via upload

---
+++ Aegisub - Lua相关学习.mm
@@ -0,0 +1,143 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Aegisub - Lua相关学习" FOLDED="false" ID="ID_100776476" CREATED="1601720331676" MODIFIED="1603907749753" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES"><hook NAME="MapStyle" background="#ffffff" zoom="1.502">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="24"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="9"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="实际操作" POSITION="right" ID="ID_1770267180" CREATED="1603907443758" MODIFIED="1603907455352">
+<node TEXT="ASS" ID="ID_1764271811" CREATED="1603907484270" MODIFIED="1603907506175">
+<node TEXT="500" OBJECT="java.lang.Long|500" ID="ID_938594711" CREATED="1603907514662" MODIFIED="1603907519946"/>
+<node TEXT="模板" ID="ID_1445796800" CREATED="1603907520590" MODIFIED="1603907825622">
+<node TEXT="自己的" ID="ID_1241250761" CREATED="1603907821534" MODIFIED="1603907829294"/>
+<node TEXT="Vmoe和老外们的" ID="ID_120614306" CREATED="1603907830207" MODIFIED="1603907840823"/>
+</node>
+</node>
+<node TEXT="LUA" ID="ID_1993998460" CREATED="1603907501782" MODIFIED="1603907513111">
+<node TEXT="1.Effector-3.5.lua" ID="ID_1124447488" CREATED="1603907534926" MODIFIED="1603907677445" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="2.Yutils.lua" ID="ID_1000887960" CREATED="1603907589990" MODIFIED="1603907605911"/>
+<node TEXT="3.Karaskel-auto4.lua" ID="ID_1693710946" CREATED="1603907607158" MODIFIED="1603907630911"/>
+<node TEXT="其他，待补充" ID="ID_491847683" CREATED="1603907644462" MODIFIED="1603907649320"/>
+</node>
+</node>
+<node TEXT="理论" POSITION="right" ID="ID_586436869" CREATED="1603907444598" MODIFIED="1603907460303">
+<node TEXT="Aegisub中文手册" ID="ID_605998627" CREATED="1603907548894" MODIFIED="1603907806412" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="Lua中文手册" ID="ID_124669121" CREATED="1603907554702" MODIFIED="1603907812140" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="Kara Effector 手册" ID="ID_1029711501" CREATED="1603908137334" MODIFIED="1603908144688"/>
+<node TEXT="视频教程" ID="ID_1312623560" CREATED="1603907561789" MODIFIED="1603907564608">
+<node TEXT="把文森特的视频教程用思维导图整理一遍" ID="ID_1996548126" CREATED="1603907569054" MODIFIED="1603907696425"/>
+</node>
+<node TEXT="图文教程" ID="ID_639521611" CREATED="1603907732102" MODIFIED="1603907735679">
+<node TEXT="知乎" ID="ID_597502820" CREATED="1603907736822" MODIFIED="1603907739311"/>
+<node TEXT="CSDN" ID="ID_1628188734" CREATED="1603907739493" MODIFIED="1603907742248"/>
+</node>
+</node>
+<node TEXT="立即去做" POSITION="right" ID="ID_1943125290" CREATED="1603907910246" MODIFIED="1603908284876" BACKGROUND_COLOR="#ff0000">
+<node TEXT="1.结合Aegisub和Lua手册凑基本语法零件，耐心点做" ID="ID_76465012" CREATED="1603908072902" MODIFIED="1603908232728"/>
+<node TEXT="2.凑到一定程度了就结合Effector-3.5.lua，转化成ASS字幕文件" ID="ID_1170823548" CREATED="1603908120150" MODIFIED="1603908214529"/>
+</node>
+</node>
+</map>
