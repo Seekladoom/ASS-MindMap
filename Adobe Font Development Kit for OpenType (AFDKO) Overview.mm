e63b0f81eef2b0256a407ee59f285d637db91c9b
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2020-10-30 11:30:04 +0800

    Add files via upload

---
+++ Adobe Font Development Kit for OpenType (AFDKO) Overview.mm
@@ -0,0 +1,156 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Adobe Font Development Kit for OpenType (AFDKO) Overview" FOLDED="false" ID="ID_100776476" CREATED="1601720331676" MODIFIED="1604023018919" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES"><hook NAME="MapStyle" background="#ffffff" zoom="0.941">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="24"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="9"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="更纱黑体 SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="1. Introduction" POSITION="right" ID="ID_1530802255" CREATED="1604023031536" MODIFIED="1604023033779">
+<node TEXT="This release of the Adobe Font Development Kit for OpenType (AFDKO) contains a set of tools used to make OpenType fonts by adding the OpenType-specific data to a TrueType or Type1 font. It does not offer tools for designing or editing glyphs. These command line programs will allow you to build an OpenType font from an existing font, verify the contents of the font, and proof the font on screen or on paper. The AFDKO also contains useful technical documentation and some example font source material. Note! Although the FDK directory tree contains a number of Python scripts, none of them can be used by double-clicking on them; they can only be called by typing commands into a command line window.&#xa;&#xa;See the afdko home page for installation instructions.&#xa;&#xa;Once the AFDKO is installed, all the command line programs can be run by entering the command name in a Terminal window (OSX, Windows, Linux), along with the necessary option text. All command line tools provide usage and help text with the options -u and -h." ID="ID_1489700790" CREATED="1604023336127" MODIFIED="1604023349395"/>
+</node>
+<node TEXT="2. Overview of Programs" POSITION="right" ID="ID_474126505" CREATED="1604023034440" MODIFIED="1604023053318">
+<node TEXT="Introduction" ID="ID_485045011" CREATED="1604024413392" MODIFIED="1604024457538">
+<node TEXT="The tools fall into several different functional groups." ID="ID_846485931" CREATED="1604024417448" MODIFIED="1604024419955"/>
+</node>
+<node TEXT="2.1 Making/editing fonts" ID="ID_937951925" CREATED="1604023053935" MODIFIED="1604023091186">
+<node TEXT="psautohint" ID="ID_204735416" CREATED="1604023126081" MODIFIED="1604023130215"/>
+<node TEXT="makeotf" ID="ID_1000995033" CREATED="1604023136304" MODIFIED="1604023145178"/>
+<node TEXT="buildmasterotfs" ID="ID_979536960" CREATED="1604023136824" MODIFIED="1604023150875"/>
+<node TEXT="buildcff2vf" ID="ID_535762886" CREATED="1604023152192" MODIFIED="1604023161616"/>
+<node TEXT="makeinstancesufo" ID="ID_933049171" CREATED="1604023152656" MODIFIED="1604023162450"/>
+<node TEXT="mergefonts" ID="ID_250395697" CREATED="1604023153120" MODIFIED="1604023166624"/>
+<node TEXT="otf2otc" ID="ID_1801675057" CREATED="1604023167511" MODIFIED="1604023172728"/>
+<node TEXT="otf2ttf" ID="ID_1419838333" CREATED="1604023173512" MODIFIED="1604023539244" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="rotatefont" ID="ID_1119770488" CREATED="1604023186096" MODIFIED="1604023191656"/>
+<node TEXT="psstemhist" ID="ID_541688497" CREATED="1604023192400" MODIFIED="1604023194682"/>
+<node TEXT="ttfcomponentizer" ID="ID_1173613751" CREATED="1604023209176" MODIFIED="1604023539247" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="ttx" ID="ID_1120789606" CREATED="1604023214208" MODIFIED="1604023539247" BACKGROUND_COLOR="#ff9900"/>
+<node TEXT="tx" ID="ID_1461136733" CREATED="1604023216959" MODIFIED="1604023220315"/>
+<node TEXT="type1/detype1" ID="ID_1476151847" CREATED="1604023225408" MODIFIED="1604023227767"/>
+</node>
+<node TEXT="2.2 Proofing" ID="ID_1515258998" CREATED="1604023055719" MODIFIED="1604023098683">
+<node TEXT="spot" ID="ID_1799466129" CREATED="1604023229519" MODIFIED="1604023239936"/>
+<node TEXT="tx" ID="ID_1040999360" CREATED="1604023230119" MODIFIED="1604023241928"/>
+<node TEXT="ttx" ID="ID_955888665" CREATED="1604023230519" MODIFIED="1604023244049"/>
+<node TEXT="charplot" ID="ID_1281130640" CREATED="1604023248655" MODIFIED="1604023252342"/>
+<node TEXT="digiplot" ID="ID_1207992878" CREATED="1604023252943" MODIFIED="1604023254242"/>
+<node TEXT="fontplot" ID="ID_30484346" CREATED="1604023258472" MODIFIED="1604023263600"/>
+<node TEXT="fontsetplot" ID="ID_943989452" CREATED="1604023264143" MODIFIED="1604023265794"/>
+<node TEXT="hintplot" ID="ID_149024997" CREATED="1604023270231" MODIFIED="1604023271634"/>
+<node TEXT="waterfallplot" ID="ID_1692283668" CREATED="1604023275672" MODIFIED="1604023277179"/>
+</node>
+<node TEXT="2.3 Validation" ID="ID_341767854" CREATED="1604023056487" MODIFIED="1604023114323">
+<node TEXT="psautohint" ID="ID_137171977" CREATED="1604023280391" MODIFIED="1604023285895"/>
+<node TEXT="checkoutlinesufo" ID="ID_822063351" CREATED="1604023286423" MODIFIED="1604023291891"/>
+<node TEXT="comparefamily" ID="ID_717485487" CREATED="1604023292824" MODIFIED="1604023297402"/>
+<node TEXT="sfntdiff" ID="ID_1975039803" CREATED="1604023302239" MODIFIED="1604023303986"/>
+<node TEXT="ttx" ID="ID_1322111303" CREATED="1604023307359" MODIFIED="1604023309591"/>
+<node TEXT="ttxn" ID="ID_104544462" CREATED="1604023310504" MODIFIED="1604023315510"/>
+<node TEXT="tx" ID="ID_1816871788" CREATED="1604023319839" MODIFIED="1604023325026"/>
+</node>
+</node>
+</node>
+</map>
