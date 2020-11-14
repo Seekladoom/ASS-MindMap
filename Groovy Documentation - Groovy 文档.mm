b6fbbeaaf93798879981328b7840d8e74a402680
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2020-11-14 23:02:45 +0800

    Add files via upload

---
+++ Groovy Documentation - Groovy 文档.mm
@@ -0,0 +1,155 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Groovy Documentation - Groovy 文档" FOLDED="false" ID="ID_100776476" CREATED="1601720331676" MODIFIED="1605360594214" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES"><hook NAME="MapStyle" background="#ffffff" zoom="0.642">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="24"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="9"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Getting started - 入门" POSITION="right" ID="ID_388200123" CREATED="1601782742280" MODIFIED="1605360665932">
+<node TEXT="Download Groovy - 下载Groovy" ID="ID_1049550222" CREATED="1601783079215" MODIFIED="1605361526930"/>
+<node TEXT="Install Groovy - 安装Groovy" ID="ID_420297043" CREATED="1601783082810" MODIFIED="1605361531010"/>
+<node TEXT="Differences with Java - 与Java的差异" ID="ID_1944367807" CREATED="1605360636460" MODIFIED="1605361535578"/>
+<node TEXT="The Groovy Development Kit - Groovy开发套件" ID="ID_1943569463" CREATED="1605360636543" MODIFIED="1605361540322"/>
+<node TEXT="Runtime and compile-time metaprogramming - 运行时和编译时元编程" ID="ID_1584991536" CREATED="1605360636630" MODIFIED="1605361545362"/>
+<node TEXT="The Grape dependency manager - 葡萄依赖管理器" ID="ID_1093737744" CREATED="1605360636722" MODIFIED="1605361550921"/>
+<node TEXT="Testing guide - 测试指南" ID="ID_1623676538" CREATED="1605360635960" MODIFIED="1605361555546"/>
+<node TEXT="Domain-Specific Languages - 领域特定语言" ID="ID_372954904" CREATED="1605360657523" MODIFIED="1605361560274"/>
+<node TEXT="Integrating Groovy into applications - 将Groovy集成到应用程序中" ID="ID_1754094902" CREATED="1605360656568" MODIFIED="1605361565482"/>
+<node TEXT="Security - 安全" ID="ID_212209786" CREATED="1605360657000" MODIFIED="1605361628628"/>
+<node TEXT="Design patterns in Groovy - Groovy中的设计模式" ID="ID_1795388906" CREATED="1605360656232" MODIFIED="1605361648875"/>
+<node TEXT="Style guide - 样式指南" ID="ID_1154150438" CREATED="1605360679945" MODIFIED="1605361769280"/>
+</node>
+<node TEXT="Groovy module guides - Groovy模块指南" POSITION="right" ID="ID_431780685" CREATED="1601783188100" MODIFIED="1605360536405">
+<node TEXT="Parsing and producing JSON - 解析并生成JSON" ID="ID_1871955115" CREATED="1601783191759" MODIFIED="1605361917770"/>
+<node TEXT="Working with a relational database - 使用关系数据库" ID="ID_981087746" CREATED="1601783193058" MODIFIED="1605361922018"/>
+<node TEXT="Processing XML - 处理XML" ID="ID_952523795" CREATED="1605360691936" MODIFIED="1605361926634"/>
+<node TEXT="Processing YAML - 处理YAML" ID="ID_1904613045" CREATED="1605360692468" MODIFIED="1605361932082"/>
+<node TEXT="Scripting Ant tasks - 编写Ant任务脚本" ID="ID_1672952080" CREATED="1605360692544" MODIFIED="1605361937386"/>
+<node TEXT="Template engines - 模板引擎" ID="ID_756024136" CREATED="1605360692625" MODIFIED="1605361943669"/>
+<node TEXT="Creating Swing UIs - 创建Swing用户界面" ID="ID_1433950525" CREATED="1605360692715" MODIFIED="1605361945642"/>
+<node TEXT="Servlet support - Servlet支持" ID="ID_1591194888" CREATED="1605360692798" MODIFIED="1605361951682"/>
+<node TEXT="Working with JMX - 使用JMX" ID="ID_965825600" CREATED="1605360726480" MODIFIED="1605361957698"/>
+</node>
+<node TEXT="Language Specification - 语言规范" POSITION="right" ID="ID_1391741946" CREATED="1601782864472" MODIFIED="1605360544954">
+<node TEXT="Syntax - 句法" ID="ID_770363361" CREATED="1601782887463" MODIFIED="1605361966378"/>
+<node TEXT="Operators - 经营者" ID="ID_1347927421" CREATED="1601782888314" MODIFIED="1605361968506"/>
+<node TEXT="Program structure - 程序结构" ID="ID_426886487" CREATED="1605360736299" MODIFIED="1605361973274"/>
+<node TEXT="Object orientation - 面向对象" ID="ID_646662452" CREATED="1605360736390" MODIFIED="1605361977090"/>
+<node TEXT="Closures - 关闭" ID="ID_1099430292" CREATED="1605360735696" MODIFIED="1605361981234"/>
+<node TEXT="Semantics - 语义学" ID="ID_303602243" CREATED="1605360736229" MODIFIED="1605361985818"/>
+</node>
+<node TEXT="Tools - 工具" POSITION="right" ID="ID_520700906" CREATED="1605360412744" MODIFIED="1605360558938">
+<node TEXT="groovyc - the Groovy compiler - groovyc-Groovy编译器" ID="ID_1018398658" CREATED="1605360763912" MODIFIED="1605361993946"/>
+<node TEXT="groovysh - the Groovy repl-like shell - groovysh-Groovy类似于repl的外壳" ID="ID_419833668" CREATED="1605360768009" MODIFIED="1605361995307"/>
+<node TEXT="groovyConsole - the Groovy Swing console - groovyConsole-Groovy Swing控制台" ID="ID_11226893" CREATED="1605360768481" MODIFIED="1605361999826"/>
+<node TEXT="IDE integration - IDE整合" ID="ID_1459317216" CREATED="1605360768985" MODIFIED="1605362004466"/>
+</node>
+<node TEXT="API documentation - API文档" POSITION="right" ID="ID_973160794" CREATED="1605360413184" MODIFIED="1605360579539">
+<node TEXT="GroovyDoc documentation of the Groovy APIs - Groovy API的GroovyDoc文档" ID="ID_1034069123" CREATED="1605360783232" MODIFIED="1605362013026"/>
+<node TEXT="The Groovy Development Kit enhancements - Groovy开发套件增强功能" ID="ID_96789867" CREATED="1605360784609" MODIFIED="1605362014914"/>
+</node>
+</node>
+</map>
