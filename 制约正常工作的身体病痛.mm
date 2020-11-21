bddbd426e87e472d40a12db5c996fd6063cead0a
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2020-11-21 22:49:18 +0800

    Add files via upload

---
+++ 制约正常工作的身体病痛.mm
@@ -0,0 +1,221 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="制约正常工作的身体病痛" FOLDED="false" ID="ID_100776476" CREATED="1601720331676" MODIFIED="1605919464072" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES"><hook NAME="MapStyle" background="#ffffff">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="24"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="9"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="咳嗽" POSITION="right" ID="ID_388200123" CREATED="1601782742280" MODIFIED="1605919315262">
+<node TEXT="具体表现" ID="ID_1049550222" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="剧烈咳嗽，且经常喘不上气来" ID="ID_1062958980" CREATED="1605964378941" MODIFIED="1605964399474"/>
+</node>
+<node TEXT="原因" ID="ID_420297043" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="肉体：饮食习惯口味明显偏重且辛辣咸味居多，缺乏运动" ID="ID_395474347" CREATED="1605964408532" MODIFIED="1605964615662"/>
+<node TEXT="精神：长期与人争斗，急火攻心" ID="ID_454545574" CREATED="1605964409764" MODIFIED="1605964493736"/>
+</node>
+<node TEXT="应对方法" ID="ID_147340029" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_555402247" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="多喝热水，该咳的时候多咳几下" ID="ID_67775947" CREATED="1605964631172" MODIFIED="1605964684231"/>
+</node>
+<node TEXT="长期处理" ID="ID_1121369407" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="调整饮食习惯，多运动" ID="ID_864685417" CREATED="1605964638972" MODIFIED="1605964649007"/>
+</node>
+</node>
+</node>
+<node TEXT="口腔溃疡" POSITION="right" ID="ID_847960830" CREATED="1605964459916" MODIFIED="1605964464070">
+<node TEXT="具体表现" ID="ID_1740617997" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="口腔内溃烂，情况严重时溃疡处还会出血。虽然现在很少溃疡了，但其实转变成咳嗽了" ID="ID_615469126" CREATED="1605964690084" MODIFIED="1605964783462"/>
+</node>
+<node TEXT="原因" ID="ID_1382503410" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="肉体：饮食习惯口味明显偏重且辛辣咸味居多，缺乏运动" ID="ID_534699470" CREATED="1605964408532" MODIFIED="1605964615662"/>
+<node TEXT="精神：长期与人争斗，急火攻心。时间长了导致身体机能下降。" ID="ID_1691374171" CREATED="1605964409764" MODIFIED="1605964813137"/>
+</node>
+<node TEXT="应对方法" ID="ID_346107739" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_1807898200" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="少食辛辣多喝水，必要的时候可以换其他淡口味的饮食方式替代" ID="ID_765401625" CREATED="1605964917972" MODIFIED="1605965098796"/>
+</node>
+<node TEXT="长期处理" ID="ID_1965463215" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="调整饮食习惯" ID="ID_1418957860" CREATED="1605964918708" MODIFIED="1605964977396"/>
+</node>
+</node>
+</node>
+<node TEXT="牙疼" POSITION="right" ID="ID_431780685" CREATED="1601783188100" MODIFIED="1605919318653">
+<node TEXT="具体表现" ID="ID_1391031919" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="牙齿底部隐隐作痛，且会引发头痛" ID="ID_1422238965" CREATED="1605964829524" MODIFIED="1605964992726"/>
+</node>
+<node TEXT="原因" ID="ID_663182833" CREATED="1601783082810" MODIFIED="1605964862409">
+<node TEXT="没有养成好好刷牙的习惯，且饮食辛辣，经常喝冷水" ID="ID_1076987881" CREATED="1605964861748" MODIFIED="1605965149718"/>
+</node>
+<node TEXT="应对方法" ID="ID_1968874916" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_1123057579" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="吃止痛药，原地小跑100下迅速转移注意力，不行就再来100下" ID="ID_529944512" CREATED="1605965294148" MODIFIED="1605965335583"/>
+</node>
+<node TEXT="长期处理" ID="ID_1494226867" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="好好刷牙，多喝热水" ID="ID_1075719173" CREATED="1605965157884" MODIFIED="1605965163638"/>
+</node>
+</node>
+</node>
+<node TEXT="干呕" POSITION="right" ID="ID_1391741946" CREATED="1601782864472" MODIFIED="1605919326870">
+<node TEXT="具体表现" ID="ID_1609102069" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="坐在电脑前，也没有情绪波动，突然就剧烈咳嗽并呕吐" ID="ID_661402823" CREATED="1605965344284" MODIFIED="1605965374902"/>
+</node>
+<node TEXT="原因" ID="ID_693211569" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="这个是长期负面情绪积累下的身体潜在病痛，人在电脑前连续工作时间长了难免发作" ID="ID_1820045512" CREATED="1605965654004" MODIFIED="1605966127127"/>
+</node>
+<node TEXT="应对方法" ID="ID_430243376" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_1162671400" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="顺势干呕，不要憋着，然后喝水" ID="ID_1889227828" CREATED="1605965778052" MODIFIED="1605965793198"/>
+</node>
+<node TEXT="长期处理" ID="ID_117654400" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="花时间调查容易让自己身体落下疾病的各种潜在因素，并慢慢改变习惯" ID="ID_1540280197" CREATED="1605965794724" MODIFIED="1605965834789"/>
+</node>
+</node>
+</node>
+<node TEXT="头痛" POSITION="right" ID="ID_472798589" CREATED="1605964262146" MODIFIED="1605964275299">
+<node TEXT="具体表现" ID="ID_439448864" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="大多痛在太阳穴位置，不是很剧烈的疼痛但足以分散人的注意力，无形中降低了工作效率" ID="ID_676804486" CREATED="1605966157140" MODIFIED="1605966387110"/>
+</node>
+<node TEXT="原因" ID="ID_506724989" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="用脑过度时容易头痛，牙痛时也会引发头痛" ID="ID_921370042" CREATED="1605966392420" MODIFIED="1605966658791"/>
+</node>
+<node TEXT="应对方法" ID="ID_1798217910" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_708380435" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="适当少想点问题，多去做事并按时作息" ID="ID_395334715" CREATED="1605968143276" MODIFIED="1605968157127"/>
+</node>
+<node TEXT="长期处理" ID="ID_765320093" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="慢慢调整生活习惯，把大脑用来思考和解决问题，而不是过于计较各种琐事" ID="ID_1185578964" CREATED="1605968159628" MODIFIED="1605968197424"/>
+</node>
+</node>
+</node>
+<node TEXT="心脏痛" POSITION="right" ID="ID_780543124" CREATED="1605964265438" MODIFIED="1605964284270">
+<node TEXT="具体表现" ID="ID_7354051" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="突然胸口剧烈疼痛，必要时得捂住心脏" ID="ID_990482465" CREATED="1605968605621" MODIFIED="1605968631534"/>
+</node>
+<node TEXT="原因" ID="ID_886533057" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="跟人争斗时产生的负面情绪积累时间长了，容易出现这种情况" ID="ID_467320162" CREATED="1605968633140" MODIFIED="1605968771895"/>
+</node>
+<node TEXT="应对方法" ID="ID_1396166812" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="慢慢改变生活习惯，不要事事都跟人急" ID="ID_7919042" CREATED="1605968781821" MODIFIED="1605968843193"/>
+</node>
+</node>
+<node TEXT="肾痛" POSITION="right" ID="ID_231735920" CREATED="1605964263356" MODIFIED="1605964294336">
+<node TEXT="具体表现" ID="ID_1859433916" CREATED="1601783079215" MODIFIED="1605919367621">
+<node TEXT="腹部疼痛，必要时需要捂住腹部" ID="ID_1934826351" CREATED="1605964498140" MODIFIED="1605968892009"/>
+</node>
+<node TEXT="原因" ID="ID_532533025" CREATED="1601783082810" MODIFIED="1605919361126">
+<node TEXT="嘛，这个就是撸多了，弟兄们少看点手冲玩意" ID="ID_1443259136" CREATED="1605968893996" MODIFIED="1605968929575"/>
+</node>
+<node TEXT="应对方法" ID="ID_688650065" CREATED="1605919391035" MODIFIED="1605919412837">
+<node TEXT="短期应急" ID="ID_1826722444" CREATED="1605919414491" MODIFIED="1605919421812">
+<node TEXT="原地小跑100下，如果实在忍不住了就冲一发吧，能不冲并用其他方式缓解的尽量用其他方式。" ID="ID_236331762" CREATED="1605968939981" MODIFIED="1605969136284"/>
+</node>
+<node TEXT="长期处理" ID="ID_444046558" CREATED="1605919422843" MODIFIED="1605919435317">
+<node TEXT="如果深刻认识到自己是个自制力比较差的人，该远离色图群和其他有色情信息的地方时，尽早远离吧。并用其他相对正常的方式，比如各种二次创作或者是其他事情上的努力多多尝试，尽可能快地找到能带来长期作用的心理正反馈。" ID="ID_606005844" CREATED="1605969023997" MODIFIED="1605969189727"/>
+</node>
+</node>
+</node>
+</node>
+</map>
