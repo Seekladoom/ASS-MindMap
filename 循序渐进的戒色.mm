7d6fc4df9657806a799b0165bad28b42b463bd00
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2021-03-20 21:42:10 +0800

    Add files via upload

---
+++ 循序渐进的戒色.mm
@@ -0,0 +1,152 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="循序渐进的戒色" FOLDED="false" ID="ID_100776476" CREATED="1601720331676" MODIFIED="1616246196215" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES"><hook NAME="MapStyle" background="#ecf4f9" zoom="1.252">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="false" edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="24"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="9"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
+<edge STYLE="horizontal" COLOR="#35455b"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="2" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="需要远离的部分和进一步的行动" POSITION="right" ID="ID_388200123" CREATED="1601782742280" MODIFIED="1616245449106">
+<node TEXT="需要远离的部分" ID="ID_1049550222" CREATED="1601783079215" MODIFIED="1616245465861">
+<node TEXT="色图" ID="ID_1855717737" CREATED="1616245476375" MODIFIED="1616245485459"/>
+<node TEXT="黄网" ID="ID_898535397" CREATED="1616245481528" MODIFIED="1616245487526"/>
+<node TEXT="短视频" ID="ID_777592649" CREATED="1616245479102" MODIFIED="1616245490700"/>
+</node>
+<node TEXT="进一步的行动" ID="ID_420297043" CREATED="1601783082810" MODIFIED="1616245474165">
+<node TEXT="色图：主动点击不感兴趣，屏蔽色图群的群消息" ID="ID_989460491" CREATED="1616245476375" MODIFIED="1616246023481" BACKGROUND_COLOR="#ff6600"/>
+<node TEXT="黄网：不去搜索和浏览" ID="ID_1551680479" CREATED="1616245481528" MODIFIED="1616245523115"/>
+<node TEXT="短视频：主动点击不感兴趣" ID="ID_270995460" CREATED="1616245479102" MODIFIED="1616245530796"/>
+</node>
+</node>
+<node TEXT="心态补位" POSITION="right" ID="ID_431780685" CREATED="1601783188100" MODIFIED="1616245556732">
+<node TEXT="备选" ID="ID_1871955115" CREATED="1601783191759" MODIFIED="1616245708200">
+<node TEXT="字幕相关" ID="ID_1090859739" CREATED="1616245714560" MODIFIED="1616245722736">
+<node TEXT="Aegisub和TCAX的相关开发工作" ID="ID_1554236611" CREATED="1616245799555" MODIFIED="1616246038400" BACKGROUND_COLOR="#ff6600"/>
+<node TEXT="OPED歌词特效的制作" ID="ID_971214702" CREATED="1616245808441" MODIFIED="1616246044839" BACKGROUND_COLOR="#ff6600"/>
+<node TEXT="给兴趣相关的朋友做字幕" ID="ID_442103590" CREATED="1616245820808" MODIFIED="1616245833508"/>
+</node>
+<node TEXT="兴趣" ID="ID_1700644670" CREATED="1616245727100" MODIFIED="1616245772013">
+<node TEXT="纪录片：学习相关知识" ID="ID_981943947" CREATED="1616245749261" MODIFIED="1616246060013"/>
+<node TEXT="动画：把握人生+消遣+相关二次创作" ID="ID_1400618409" CREATED="1616245773375" MODIFIED="1616246472057" BACKGROUND_COLOR="#ff6600"/>
+<node TEXT="电视剧：把握人生+消遣" ID="ID_1914927787" CREATED="1616245763904" MODIFIED="1616246126468"/>
+<node TEXT="日剧：把握人生+消遣" ID="ID_1174772824" CREATED="1616245790256" MODIFIED="1616246131822"/>
+<node TEXT="知乎：把握人生+消遣" ID="ID_1484821993" CREATED="1616246218421" MODIFIED="1616246254561" BACKGROUND_COLOR="#ff6600">
+<node TEXT="把握人生（70%）：字幕制作相关+其他社会经验" ID="ID_572276316" CREATED="1616246239444" MODIFIED="1616246398875"/>
+<node TEXT="消遣（30%）：看看自己相关领域内的段子等" ID="ID_1951999031" CREATED="1616246247333" MODIFIED="1616246457190"/>
+</node>
+<node TEXT="CSDN" ID="ID_528047503" CREATED="1616246267763" MODIFIED="1616246270506">
+<node TEXT="学习：Lua和Python相关的有利于直接制作字幕的经验等" ID="ID_669897424" CREATED="1616246453642" MODIFIED="1616246529598"/>
+<node TEXT="输出：发Aegisub和TCAX相关的教程" ID="ID_1689929045" CREATED="1616246494185" MODIFIED="1616246545463"/>
+</node>
+</node>
+</node>
+<node TEXT="正反馈" ID="ID_981087746" CREATED="1601783193058" MODIFIED="1616245685296">
+<node TEXT="1.OPED歌词特效对他人和自己内心上的鼓舞" ID="ID_1923708178" CREATED="1616245868637" MODIFIED="1616246168446"/>
+<node TEXT="2.开发工作在能力和心理上带来的进步" ID="ID_8231123" CREATED="1616245891690" MODIFIED="1616246565793" BACKGROUND_COLOR="#ff6600"/>
+<node TEXT="3.参与相关活动赚到钱，逐步减少家人的妨碍" ID="ID_1905436189" CREATED="1616245903965" MODIFIED="1616246171823"/>
+</node>
+</node>
+</node>
+</map>
